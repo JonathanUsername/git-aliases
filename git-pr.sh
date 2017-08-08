762094e43c04e7bced73032898b8ce3760e10d47
Jonathan King <jonathan.j.king@gmail.com>
2017-08-08 11:49:30 +0100

    Open PR directly

--- git-pr.sh
+++ git-pr.sh
@@ -2,11 +2,12 @@
 
 REPO=$(git remote get-url origin | sed -E 's/^git@github.com:|\.git$//g')
 
-TOKEN="$(cat ~/secrets.json | jq -r .token)"
+TOKEN="$(jq -r .token ~/secrets.json)"
 URL="http://api.github.com/repos/$REPO/pulls?access_token=$TOKEN"
 BRANCH="$(git symbolic-ref --quiet --short HEAD)"
 JQ=".[] | select(.head.ref == \"$BRANCH\") | ._links.html.href"
 
 URL=$(curl -sL "$URL" | jq -r "$JQ")
+COMPARE_URL="https://github.com/$REPO/compare/$BRANCH?expand=1"
 
-[[ -n "$URL" ]] && open "$URL" || echo "No PR found for $BRANCH in github.com/$REPO."
+[[ -n "$URL" ]] && open "$URL" || open "$COMPARE_URL"
